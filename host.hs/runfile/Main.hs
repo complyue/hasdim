882171bf458a6847ef6b5e367430ae0784763d18
ComplYue <compl.yue@icloud.com>
2021-06-21 22:07:50 +0800

    runfile/runmodu upd

--- host.hs/runfile/Main.hs
+++ host.hs/runfile/Main.hs
@@ -2,13 +2,10 @@ module Main where
 
 -- import           Debug.Trace
 
-import Control.Concurrent (forkFinally)
-import Control.Exception (SomeException)
-import Control.Monad
-import qualified Data.Text as T
-import Dim.EHI (installDimBatteries)
+import Dim.EHI
 import Language.Edh.EHI
-import Language.Edh.Net (installNetBatteries)
+import Language.Edh.Net
+import Language.Edh.Run
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
@@ -17,43 +14,11 @@ import Prelude
 main :: IO ()
 main =
   getArgs >>= \case
-    [!edhFile] -> do
-      !console <- defaultEdhConsole defaultEdhConsoleSettings
-      let !consoleOut = consoleIO console . ConsoleOut
-          runIt = do
-            world <- createEdhWorld console
-            installEdhBatteries world
+    [edhFile] -> edhRunFile defaultEdhConsoleSettings edhFile $ \ !world -> do
+      -- install all necessary batteries
+      installEdhBatteries world
+      installNetBatteries world
+      installDimBatteries world
 
-            -- install batteries provided by nedh
-            installNetBatteries world
-
-            -- install batteries provided by hasdim
-            installDimBatteries world
-
-            runEdhFile world edhFile >>= \case
-              Left !err -> do
-                -- program crash on error
-                consoleOut "HasDim crashed with an error:\n"
-                consoleOut $ T.pack $ show err <> "\n"
-              Right !phv -> case edhUltimate phv of
-                -- clean program halt, all done
-                EdhNil -> return ()
-                -- unclean program exit
-                _ -> do
-                  consoleOut "HasDim halted with a result:\n"
-                  consoleOut $
-                    (<> "\n") $ case phv of
-                      EdhString msg -> msg
-                      _ -> T.pack $ show phv
-
-      void $
-        forkFinally runIt $ \ !result -> do
-          case result of
-            Left (e :: SomeException) ->
-              consoleOut $ "💥 " <> T.pack (show e)
-            Right _ -> pure ()
-          -- shutdown console IO anyway
-          consoleIO console ConsoleShutdown
-
-      consoleIOLoop console
+    --
     _ -> hPutStrLn stderr "Usage: rundim <.edh-file>" >> exitFailure
